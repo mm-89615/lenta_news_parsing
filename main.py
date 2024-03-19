7971fd7c1cdf5dd2fef21f1a8774796d9f00dab4
Maxim Molyanov <mm_89615@hotmail.com>
2024-03-19 20:17:41 +0300

    parsing all news for the day

--- main.py
+++ main.py
@@ -3,42 +3,42 @@ import requests
 import json
 
 
-url = 'https://lenta.ru/2024/03/19/'
-
-page = requests.get(url)
-
-filtered_news = []
-all_news = []
-
-soup = bs(page.text, "html.parser")
-
-all_news = soup.find_all("li", class_='archive-page__item _news')
-
-
 def get_news():
-    for data in all_news:
-        title_news = data.find(
-            'h3',
-            class_='card-full-news__title')
-        publication_time = data.find(
-            'time',
-            class_='card-full-news__info-item card-full-news__date')
-        category_news = data.find(
-            'span',
-            class_='card-full-news__info-item card-full-news__rubric')
-        url = data.find('a', href=True)
+    page_number = 1
+    filtered_news = []
+    while True:
+        page_number += 1
+        url = f'https://lenta.ru/2024/03/19/page/{page_number}/'
+        page = requests.get(url)
+        soup = bs(page.text, "html.parser")
+        all_news = soup.find_all("li", class_='archive-page__item _news')
+        if len(all_news) == 0:
+            break
+        else:
+            for data in all_news:
+                title_news = data.find(
+                    'h3',
+                    class_='card-full-news__title')
+                publication_time = data.find(
+                    'time',
+                    class_='card-full-news__info-item card-full-news__date')
+                category_news = data.find(
+                    'span',
+                    class_='card-full-news__info-item card-full-news__rubric')
+                url = data.find('a', href=True)
 
-        news = {
-            'title': title_news.text,
-            'publication_time': publication_time.text,
-            'category': category_news.text,
-            'url': "https://lenta.ru" + url['href']
-        }
+                news = {
+                    'title': title_news.text,
+                    'publication_time': publication_time.text,
+                    'category': category_news.text,
+                    'url': "https://lenta.ru" + url['href']
+                }
 
-        filtered_news.append(news)
+                filtered_news.append(news)
 
-    with open('filtered_news.json', 'w', encoding='UTF-8') as file:
-        json.dump(filtered_news, file, indent=4, ensure_ascii=False)
+        with open('filtered_news.json', 'w', encoding='UTF-8') as file:
+            json.dump(filtered_news, file, indent=4, ensure_ascii=False)
+
 
 def main():
     get_news()
